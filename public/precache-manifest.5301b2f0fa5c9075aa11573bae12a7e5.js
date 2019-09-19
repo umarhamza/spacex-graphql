f5b74653fee94a35cb623ecce59df181e3f2c8b9
umarhamza-cs <umar.hamza@clubspark.com>
2019-09-19 18:53:03 +0100

    building files

---
+++ public/precache-manifest.5301b2f0fa5c9075aa11573bae12a7e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73b80e716ebc5ef2bccab2f64fe032d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b1eb2d3003a4a798205",
+    "url": "/static/css/main.90e4acc6.chunk.css"
+  },
+  {
+    "revision": "3557bbc0eee26ff77cba",
+    "url": "/static/js/2.96f6041e.chunk.js"
+  },
+  {
+    "revision": "2b1eb2d3003a4a798205",
+    "url": "/static/js/main.2569977b.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "e4f6f1ebd26cd6aa71a11a20f900d3dd",
+    "url": "/static/media/bg.e4f6f1eb.jpg"
+  },
+  {
+    "revision": "dc45d3dff453115dbe0d23b384f34738",
+    "url": "/static/media/logo-black.dc45d3df.png"
+  }
+]);
